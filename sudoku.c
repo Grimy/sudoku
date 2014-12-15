6848f9932e1d317b45468965c9f91aa6d6f1aa32
Victor Adam <Victor.Adam@derpymail.org>
2014-12-15 07:03:32 +0100

    Added an option to find multiple solutions

--- sudoku.c
+++ sudoku.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "graph.c"
 
+#define ASSUME_SINGLE_SOLUTION 1
 static char grid[82] = {0};
 
 /* Returns 1 if the grid is in a valid state, 0 otherwise */
@@ -12,18 +13,7 @@ static int check_grid(long pos) {
 	return 1;
 }
 
-/* Tries to fill the grid with a solution . Returns 1 on success,
-   0 on failure (requires backtracking) */
-static int solve_grid(long pos) {
-	if (grid[pos] != ':')
-		return grid[pos] ? solve_grid(pos + 1) : 1;
-	for (grid[pos] = '1';  grid[pos] <= '9'; ++grid[pos])
-		if (check_grid(pos) && solve_grid(pos + 1))
-			return 1;
-	return 0;
-}
-
-static void display_grid() {
+static int display_grid() {
 	static const char* const formats[] = {
 		"╔═╤═╤═╦═╤═╤═╦═╤═╤═╗",
 		"╟━╋━╋━╫━╋━╋━╫━╋━╋━╢",
@@ -39,16 +29,25 @@ static void display_grid() {
 	char *r = grid;
 	for (long row = 0; row < 9; ++row, r += 9)
 		printf("%s\n║%c┃%c┃%c║%c┃%c┃%c║%c┃%c┃%c║\n", formats[row],
-			r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8]);
+				r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8]);
 	printf("%s\n", formats[9]);
+	return ASSUME_SINGLE_SOLUTION;
+}
+
+/* Tries to fill the grid with a solution . Returns 1 on success,
+   0 on failure (requires backtracking) */
+static int solve_grid(long pos) {
+	if (grid[pos] != ':')
+		return grid[pos] ? solve_grid(pos + 1) : display_grid();
+	for (grid[pos] = '1';  grid[pos] <= '9'; ++grid[pos])
+		if (check_grid(pos) && solve_grid(pos + 1))
+			return 1;
+	return 0;
 }
 
 int main(void) {
 	fgets(grid, sizeof(grid), stdin);
-	int solved = solve_grid(0);
-	if (solved)
-		display_grid();
-	else
+	if (!solve_grid(0) && ASSUME_SINGLE_SOLUTION)
 		printf("No solution!\n");
-	return !solved;
+	return 0;
 }
